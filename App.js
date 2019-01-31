4bb69bbd0542b013f1c3360e602ecaf73aec9535
Baochuan Lu <lubaochuan@gmail.com>
2019-01-31 10:44:47 -0600

    factor out SearchInput

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Platform, TextInput, KeyboardAvoidingView }
   from 'react-native';
+import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
   render() {
@@ -17,13 +18,7 @@ export default class App extends React.Component {
         <Text style={[styles.largeText, styles.textStyle]}>
           24°
         </Text>
-        <TextInput
-          autoCorrect={false}
-          placeholder="Search any city"
-          placeholderTextColor="white"
-          style={styles.textInput}
-          clearButtonMode="always"
-          />
+        <SearchInput placeholder="Search any city" />
       </KeyboardAvoidingView>
     );
   }
@@ -46,15 +41,5 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 18,
-  },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
-  },
+  }
 });
